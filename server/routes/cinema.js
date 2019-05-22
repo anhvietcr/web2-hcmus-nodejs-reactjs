a240654336108d847abb61703ab22884f8df7f1a
nploi <nploi1998@gmail.com>
2019-05-22 15:42:44 +0700

    Fix format response

--- server/routes/cinema.js
+++ server/routes/cinema.js
@@ -4,21 +4,52 @@ let router = new Router();
 
 /***************HOME API ******************/
 router.get('/', async (req, res, next) => {
-    return await Cinema.findAll().then((result) => res.json(result));
+    const cinemas = await Cinema.findAll();
+    var status = 200;
+    var message = '';
+
+    if (!cinemas) {
+        status = 404;
+        message = 'Not found';
+    }
+
+    return res.json({
+        status: status,
+        message: message,
+        payload: {
+            cinemas: cinemas
+        }
+    });
 });
 
 router.get('/:id', async (req, res, next) => {
-    return await Cinema.findAll({
+    const cinemas = await Cinema.findAll({
         where: {
             id: req.params.id
         }
-    }).then((result) => res.json(result));
+    });
+
+    var status = 200;
+    var message = '';
+
+    if (!cinemas) {
+        status = 404;
+        message = 'Not found';
+    }
+
+    return res.json({
+        status: status,
+        message: message,
+        payload: {
+            cinemas: cinemas
+        }
+    });
 });
 
 router.post('/', async (req, res, next) => {
     const created_at = new Date();
     const newCinema = req.body.cinema;
-    return await Cinema.create({
+    const cinema = await Cinema.create({
         name: newCinema.name,
         image: newPost.image,
         trailer: newCinema.trailer,
@@ -27,16 +58,29 @@ router.post('/', async (req, res, next) => {
         minute_time: newCinema.minute_time,
         view: newCinema.view,
         created_at: created_at
-    })
-        .then(post => {
-            res.json(post);
-        });
+    });
+
+    var status = 200;
+    var message = '';
+
+    if (!cinema) {
+        status = 503;
+        message = 'Service Unavailable';
+    }
+
+    return res.json({
+        status: status,
+        message: message,
+        payload: {
+            cinema: cinema
+        }
+    });
 });
 
 router.put('/:id', async (req, res, next) => {
     const updated_at = new Date();
     const updateCinema = req.body.cinema;
-    return await Cinema.update({
+    const numAffectedRows = await Cinema.update({
         name: updateCinema.name,
         image: updateCinema.image,
         trailer: updateCinema.trailer,
@@ -50,15 +94,41 @@ router.put('/:id', async (req, res, next) => {
             where: {
                 id: req.params.id
             }
-        }).then((result) => res.json(result));
+        });
+
+    var status = 204;
+    var message = '';
+
+    if (numAffectedRows <= 0) {
+        status = 503;
+        message = 'Service Unavailable';
+    }
+
+    return res.json({
+        status: status,
+        message: message,
+    });
 });
 
 router.delete('/:id', async (req, res, next) => {
-    return await Cinema.destroy({
+    const numAffectedRows = await Cinema.destroy({
         where: {
             id: req.params.id
         }
-    }).then((result) => res.json(result));
+    });
+
+    var status = 204;
+    var message = '';
+
+    if (numAffectedRows <= 0) {
+        status = 503;
+        message = 'Service Unavailable';
+    }
+
+    return res.json({
+        status: status,
+        message: message,
+    });
 });
 
 module.exports = router
