ff3e55f55f52c6ef651ca6d6dc3720bef06f35bf
anhvietcr <anhvietcr20@gmail.com>
2019-05-28 15:58:43 +0700

    add UI Home

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,31 +1,96 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import { withStyles } from '@material-ui/core/styles'
 import Navbar from './head/Navbar';
-  
-const styles = (theme) => ({ 
-    root: {
-        backgroundColor: 'red'
+import Grid from '@material-ui/core/Grid'
+import CardFilm from './helper/CardFilm'
+
+const styles = (theme) => ({
+  root: {
+    backgroundColor: '#ffffff',
+    flexGrow: 1,
+  },
+  paper: {
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+  homeTitle: {
+    background: "url(../bg_h3_line.jpg) repeat-x scroll left center rgba(0, 0, 0, 0)",
+    height: "60px",
+    textAlign: "center",
+    width: "100%",
+    padding: '10px 0px',
+    "& > *": {
+      display: "inline-block",
+      width: "230px",
+      height: "40px",
+      textIndent: "-99999px",
+      margin: "10px",
+      padding: 0
     },
-})
+    "& > h2": {
+      background: "url(../h3_movie_new.gif) no-repeat scroll center center / 406px 41px #FDFCF0",
+    },
+    "& > h3": {
+      background: "url(../h3_movie_view.gif) no-repeat scroll center center / 406px 41px #FDFCF0",
+    }
+  },
+});
+
 
 const Home = (props) => {
-    const { actions, classes } = props;
+  const { actions, classes } = props;
+  const [hover, setHover] = useState(false)
 
-    return (
-        <React.Fragment>
-            <Navbar />
-
-            <div className={classes.root} onClick={() => {
-                actions.Datve("data ve");
-            }}>Connect API
-            </div>
-        </React.Fragment>
-    ) 
+  return (
+    <React.Fragment>
+      <Navbar />
+      <div className={classes.root}>
+        <div className={classes.homeTitle}>
+          <h2>movie news</h2>
+        </div>
+        <section className={classes.movies}>
+          <Grid container spacing={8}>
+            <Grid item sm={6} xs={6} md={4} lg={3}>
+              <CardFilm />
+            </Grid>
+            <Grid item sm={6} xs={6} md={4} lg={3}>
+              <CardFilm />
+            </Grid>
+            <Grid item sm={6} xs={6} md={4} lg={3}>
+              <CardFilm />
+            </Grid>
+            <Grid item sm={6} xs={6} md={4} lg={3}>
+              <CardFilm />
+            </Grid>
+          </Grid>
+        </section>
+        <div className={classes.homeTitle}>
+          <h3>movie views</h3>
+        </div>
+        <section className={classes.movies}>
+          <Grid container spacing={8}>
+            <Grid item sm={6} xs={6} md={4} lg={3}>
+              <CardFilm />
+            </Grid>
+            <Grid item sm={6} xs={6} md={4} lg={3}>
+              <CardFilm />
+            </Grid>
+            <Grid item sm={6} xs={6} md={4} lg={3}>
+              <CardFilm />
+            </Grid>
+            <Grid item sm={6} xs={6} md={4} lg={3}>
+              <CardFilm />
+            </Grid>
+          </Grid>
+        </section>
+      </div>
+    </React.Fragment>
+  )
 }
 
 Home.propTypes = {
-    classes: PropTypes.object.isRequired
+  classes: PropTypes.object.isRequired
 };
 
 export default withStyles(styles)(Home)
---
+++ client/src/components/helper/CardFilm.js
@@ -0,0 +1,114 @@
+import React, { useState } from 'react'
+import PropTypes from 'prop-types'
+import { withStyles } from '@material-ui/core/styles'
+import classNames from 'classnames'
+import Grid from '@material-ui/core/Grid'
+import Paper from '@material-ui/core/Paper'
+import Typography from '@material-ui/core/Typography'
+import Divider from '@material-ui/core/Divider'
+
+const styles = (theme) => ({
+  movieTitle: {
+    fontSize: '20px'
+  },
+  movieCard: {
+    position: "relative",
+    height: '250px',
+    maxHeight: '250px',
+    padding: theme.spacing.unit,
+    margin: theme.spacing.unit,
+
+    "& > img": {
+      width: '100%',
+      height: '85%',
+      transition: ".5s",
+      paddingBottom: '5px',
+    },
+  },
+  movieBook: {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    width: "100%",
+    transform: "translate(-50%, -50%)",
+    textAlign: "center",
+
+  },
+  btnBook: {
+    color: '#fff',
+    border: "1px solid #fff",
+    display: "inline-block",
+    fontSize: "14px",
+    letterSpacing: "1px",
+    textTransform: "uppercase",
+    color: "#ffffff",
+    fontWeight: "400",
+    padding: "15px 30px",
+    textDecoration: "none",
+    lineHeight: 1,
+
+    "&:hover": {
+      backgroundColor: "rgba(255, 0, 0, 0.7)",
+    }
+  },
+  overlap: {
+    zIndex: "99",
+    width: "100%",
+    height: "100%",
+    transition: "all 300ms",
+    opacity: 0,
+    backgroundColor: "rgba(0, 0, 0, 0.7)"
+  },
+  movieHover: {
+    position: "absolute",
+    top: 0,
+    left: 0
+  },
+  override: {
+    opacity: "1 !important"
+  }
+});
+
+const CardFilm = (props) => {
+  const { classes } = props
+  const [hover, setHover] = useState(false)
+
+  return (
+    <Paper className={classes.movieCard}>
+      <img src="movie.jpg" alt={"img"} />
+      <a href="/ticket">
+        <div
+          className={classNames(classes.overlap, classes.movieHover, hover ? classes.override : "")}
+          onMouseOver={() => {
+            setHover(true);
+          }}
+          onMouseOut={() => {
+            setHover(false)
+          }}
+        >
+          <div className={classes.movieBook}>
+            <Typography
+              variant="h5"
+              component="h3"
+              className={classes.btnBook}
+            > Đặt Vé
+              </Typography>
+          </div>
+        </div>
+      </a>
+      <Divider />
+      <Typography
+        variant="h5"
+        component="h3"
+        className={classes.movieTitle}
+      > Tên phim
+        </Typography>
+    </Paper>
+  )
+}
+
+CardFilm.propTypes = {
+  classes: PropTypes.object.isRequired
+}
+
+export default withStyles(styles)(CardFilm)
