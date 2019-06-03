478c80d09fdaa105372028c26aaebda9acb24a68
= <phanthinhutranghahl@gmail.com>
2019-06-03 16:39:53 +0700

    layout movie detail page,front-end

--- client/src/components/MovieDetail.js
+++ client/src/components/MovieDetail.js
@@ -3,109 +3,218 @@ import PropTypes from 'prop-types'
 import { withStyles } from '@material-ui/core/styles'
 import classNames from 'classnames'
 import Navbar from '../components/head/Navbar'
+import SimpleSelect from './helper/SimpleSelect'
+import SmallButton from './helper/SmallButton'
+import Grid from '@material-ui/core/Grid'
+import List from '@material-ui/core/List'
+import ListItem from '@material-ui/core/ListItem'
+import Divider from '@material-ui/core/Divider'
+import ListItemText from '@material-ui/core/ListItemText'
+import Typography from '@material-ui/core/Typography'
+import Timelapse from '@material-ui/icons/Timelapse'
 
 const styles = theme => ({
-    root: {
-        backgroundColor: '#fafafa'
-    },
-    left: {
-        position: 'relative',
-        top: 0,
-        bottom: 0,
-        left: 0,
-        right: 0
-    },
-    right: {
-        position: 'relative',
-        top: 0,
-        bottom: 0,
-        left: 0,
-        right: 0
-    },
-    movieDetail: {
-        display: 'block',
-        backgroundColor: '#ccc',
-        position: 'relative'
-    },
-    cinemasDetail: {
-        display: 'block',
-    }
+  root: {
+    backgroundColor: '#fafafa'
+  },
+  left: {
+    position: 'relative',
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0
+  },
+  right: {
+    position: 'relative',
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0
+  },
+  movieDetail: {
+    display: 'block',
+    backgroundColor: '#ccc',
+    position: 'relative'
+  },
+  smallbtn: {
+    margin: "15px 0px 0px 0px",
+    bottom: 0,
+  },
+  inline: {
+    display: 'inline',
+  },
+  divider: {
+    marginTop: '10px'
+  },
+  list: {
+    display: 'inline-block',
+    position: 'relative',
+  },
+  imgLeft: {
+    width: "100%",
+    height: '250px',
+  },
 })
 
 const MovieDetail = (props) => {
-    const { actions, classes } = props
-    const { ShowtimeCpanel } = actions
-    const movie_id = actions.match.params.id;
-    const [dataMovie, setDataMovie] = useState([])
-    const [dataCinemas, setDataCinemas] = useState([])
+  const { actions, classes } = props
+  const { ShowtimeCpanel } = actions
+  const movie_id = actions.match.params.id;
+  const [dataMovie, setDataMovie] = useState([])
+  const [dataCinemas, setDataCinemas] = useState([])
+  const [dataTheater, setDataTheater] = useState([])
+  const [dataShowtime, setDataShowtime] = useState([])
 
-    // Get showtimes by movie id
-    useEffect(() => {
-        actions.getShowTimesByMovie({ movie_id })
-    }, [movie_id])
+  // Get showtimes by movie id
+  useEffect(() => {
+    actions.getShowTimesByMovie({ movie_id })
+  }, [movie_id])
 
-    useEffect(() => {
-        if (ShowtimeCpanel.showtimes_movie) {
-            console.log(ShowtimeCpanel.showtimes_movie.payload)
+  useEffect(() => {
+    if (ShowtimeCpanel.showtimes_movie) {
+      console.log(ShowtimeCpanel.showtimes_movie.payload)
 
-            const { movie, cinemas } = ShowtimeCpanel.showtimes_movie.payload
-            setDataCinemas(cinemas)
-            setDataMovie(movie)
-        }
-    }, [ShowtimeCpanel])
-
-    // load area movie
-    const loadMovieDetail = () => {
-
-        return (
-            <section className={classes.movieDetail}>
-                <span className={classNames(classes.movieImage, classes.left)}>
-                    <img src="/movie.jpg" alt="movie trailer" />
-                </span>
-                <span className={classNames(classes.movieDetailRight, classes.right)}>
-                    <h3>{dataMovie.name}</h3>
-                    <h6>{dataMovie.introduce}</h6>
-                    <label>{dataMovie.opening_day}</label>
-                    <label>{dataMovie.minute_time} phut</label>
-                </span>
-            </section>
-        )
+      const { movie, cinemas } = ShowtimeCpanel.showtimes_movie.payload
+      setDataCinemas(cinemas)
+      setDataMovie(movie)
     }
+  }, [ShowtimeCpanel])
 
-    // load area cinemas
-    const loadShowtimeDetail = () => {
-        return (
-            <section className={classes.cinemasDetail}>
-                <span className={classNames(classes.cinemasContent, classes.left)}>
-                    c
-                </span>
-                <span className={classNames(classes.cinemaMap, classes.right)}>
-                    D
-                </span>
-            </section>
-        )
-    }
-
-    const theaterArea = (theater) => {
-        
-    }
-
-    const cinemaArea = (cinemas) => {
-        
-    }
+  // load area movie
+  const movieDetailArea = () => {
 
     return (
-        <section className={classes.root}>
-            <Navbar />
-            {dataMovie.id && loadMovieDetail()}
-            {dataCinemas.length && loadShowtimeDetail()}
-        </section>
+      <React.Fragment>
+        <Grid item xs={6} sm={6} md={6} lg={6}>
+          <img src="/movie.jpg" alt="movie trailer" />
+        </Grid>
+        <Grid item xs={6} sm={6} md={6} lg={6}>
+          <h3>{dataMovie.name}</h3>
+          <h6>{dataMovie.introduce}</h6>
+          <label>{dataMovie.opening_day}</label>
+          <label>{dataMovie.minute_time} phut</label>
+        </Grid>
+      </React.Fragment>
     )
+  }
+
+  // load area cinemas
+  const mainArea = () => {
+    return (
+      <React.Fragment>
+        <Grid item sm={12} xs={12} md={6} lg={6}>
+          {theaterArea(1)}
+        </Grid>
+        <Grid item sm={12} xs={12} md={6} lg={6}>
+          {cinemaArea(1)}
+        </Grid>
+      </React.Fragment>
+    )
+  }
+
+  const handleShowTimeClick = (e) => {
+    console.log(e.target)
+  }
+
+  const getShowtimes = showtimes => {
+    return showtimes.map((showtime) => {
+      return (
+        <SmallButton
+          key={showtime.id}
+          id={showtime.id}
+          handleSubmit={handleShowTimeClick}
+          text={showtime.start_time.split(" ")[1]} />
+      )
+    })
+  }
+
+  const theaterArea = (cinema_id) => {
+    let cinema = dataCinemas.filter((cinema) => {
+      return cinema.id === cinema_id
+    })[0];
+
+    if (typeof cinema === 'undefined' || !dataCinemas.length) {
+      return (
+        <p>Khong tim thay suat chieu trong cum rap</p>
+      )
+    }
+
+    const { theaters } = cinema;
+    return (
+      <List className={classes.root}>
+        {
+          theaters.map((theater) => {
+            return (
+                <ListItem alignItems="flex-start" key={theater.id} className={classes.list}>
+                  <Grid container spacing={8}>
+                    <Grid item sm={12} xs={12} md={12} lg={12}>
+                      <ListItemText
+                        primary={
+                          <React.Fragment>
+                            <Typography
+                              component="span"
+                              variant="h5"
+                              color="textPrimary"
+                            >
+                              {theater.name}
+                            </Typography>
+                            <Typography
+                              component="span"
+                              variant="subheading"
+                              color="textPrimary"
+                              className={classes.inline}
+                            >
+                              <Timelapse className={classes.smallicon} /> {dataMovie.minute_time + " phút"}
+                            </Typography>
+                            {" — " + theater.type}
+                          </React.Fragment>
+                        }
+                      />
+                      <span className={classes.smallbtn}>
+                        {getShowtimes(theater.showtimes)}
+                      </span>
+                    </Grid>
+                  </Grid>
+                  <Divider className={classes.divider} />
+                </ListItem>
+            )
+        })}
+      </List>
+    )
+  }
+
+  const cinemaArea = (cinema_id) => {
+    return (
+      <Grid container spacing={8}>
+        <Grid item sm={12} xs={12} md={12} lg={12}>
+          <p>Cinemas combobox</p>
+       </Grid>
+       <Grid item sm={12} xs={12} md={12} lg={12}>
+          <p>Cinema map</p>
+       </Grid>
+      </Grid>
+    )
+  }
+
+
+
+  return (
+    <section className={classes.root}>
+      <Navbar />
+      <Grid container spacing={8}>
+        {dataMovie.id && movieDetailArea()}
+      </Grid>
+      <Divider className={classes.divider}/>
+      <Grid container spacing={8}>
+        {mainArea()}
+      </Grid>
+    </section>
+  )
 }
 
 MovieDetail.propTypes = {
-    classes: PropTypes.object.isRequired,
-    actions: PropTypes.object.isRequired
+  classes: PropTypes.object.isRequired,
+  actions: PropTypes.object.isRequired
 }
 
 export default withStyles(styles)(MovieDetail)
